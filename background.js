aa47e56909640be97f4dbd45659f4f9947b8a5c3
Liang Cai <coronin@gmail.com>
2011-07-21 23:29:21 -0700

    v0.0.2

--- background.js
+++ background.js
@@ -1,64 +1,65 @@
-function loginTab(loginurl, string, cb){
-    if(typeof chrome != 'undefined'){
+function loginTab(loginurl, string, cb) {
+  if (typeof chrome !== 'undefined') {
     chrome.tabs.create({
       url: loginurl
-    }, function(tab){
-      var poll = function(){
-        chrome.tabs.get(tab.id, function(info){
-          if(info.url.indexOf(string) != -1){
+    }, function (tab) {
+      var poll = function () {
+        chrome.tabs.get(tab.id, function (info) {
+          if (info.url.indexOf(string) !== -1) {
             cb(info.url);
             chrome.tabs.remove(tab.id);
-          }else{
-            setTimeout(poll, 500)
+          } else {
+            setTimeout(poll, 500);
           }
-        })
+        });
       };
       poll();
-    })
-  }else if(typeof tabs != 'undefined'){
+    });
+  } else if (typeof tabs !== 'undefined') {
     tabs.open({
       url: loginurl,
-      onOpen: function(tab){
-        var poll = function(){
-          if(tab.url.indexOf(string) != -1){
+      onOpen: function (tab) {
+        var poll = function () {
+          if (tab.url.indexOf(string) !== -1) {
             cb(tab.url);
             tab.close();
-          }else{
-            setTimeout(poll, 500)
+          } else {
+            setTimeout(poll, 500);
           }
         };
         poll();
       }
-    })
+    });
   }
 }
 
-function https(){
-  if(localStorage.no_https == 'on'){
-    return 'http://'; //idk why someone would want this
+function https() {
+  if (localStorage.no_https === 'on') {
+    return 'http://';
   }
   return 'https://';
 }
 
-function getURL(type, request, callback, sync){
-  if(request.data && sync) return request.data;
-  
-  if(request.data) return callback(request); //no need reconverting!
-  
-  if(/^data:/.test(request.url)){
+function getURL(type, request, callback, sync) {
+  if (request.data && sync) return request.data;
+
+  if (request.data) return callback(request); // no need reconverting!
+
+  if (/^data:/.test(request.url)) {
     console.log('opened via data url');
     var parts = request.url.match(/^data:(.+),/)[1].split(';');
     var mime = parts[0], b64 = parts.indexOf('base64') != -1;
     var enc = request.url.substr(request.url.indexOf(',')+1);
     var data = b64 ? atob(enc) : unescape(enc);
-    //data urls dont have any weird encoding issue as far as i can tell
+    // data urls dont have any weird encoding issue as far as i can tell
     var name = '';
-    if(request.name){
+    if (request.name) {
       name = request.name;
-    }else{
+    } else {
       name = enc.substr(enc.length/2 - 6, 6) + '.' + mime.split('/')[1];
     }
-    if(sync) return data;
+    if (sync) return data;
+
     callback({
       data: data,
       type: mime,
@@ -66,74 +67,72 @@ function getURL(type, request, callback, sync){
       size: data.length,
       name: name, url: request.url
     });
-    
+
     //callback(new dFile(data, name, mime, id, size)
   }else{
     
     var xhr = new XMLHttpRequest();
-    xhr.addEventListener('progress', function(evt){
-          downloadProgress(request.url, evt);
-      }, false)
-  
+    xhr.addEventListener('progress', function (evt) {
+        downloadProgress(request.url, evt);
+      }, false);
+
     xhr.open('GET', request.url, !sync);
-    if(type == 'binary' || type == 'raw'){
-      xhr.overrideMimeType('text/plain; charset=x-user-defined'); //should i loop through and do that & 0xff?
+    if (type === 'binary' || type === 'raw') {
+      xhr.overrideMimeType('text/plain; charset=x-user-defined'); // should i loop through and do that & 0xff?
     }
-    if(type == 'arraybuffer'){
-        console.log('Setting Type ArrayBuffer');
-            xhr.responseType = 'arraybuffer';
-        }
-    
-    if(sync){
+    if (type === 'arraybuffer') {
+      console.log('Setting Type ArrayBuffer');
+      xhr.responseType = 'arraybuffer';
+    }
+
+    if (sync) {
       xhr.send();
       return xhr.responseText;
     }
-    xhr.onload = function(){
+    xhr.onload = function () {
       if(!request.type) request.type = xhr.getResponseHeader("Content-Type");
     
       console.log('opened via xhr ', request.url);
       var data = '';
 
-
-      if(type == 'binary'){
+      if(type === 'binary'){
         //*
-        if(typeof BlobBuilder == 'undefined'){
-        
-          for(var raw = xhr.responseText, l = raw.length, i = 0, data = ''; i < l; i++) data += String.fromCharCode(raw.charCodeAt(i) & 0xff);
-          
+
+        if(typeof BlobBuilder === 'undefined'){
+          for (var raw = xhr.responseText, l = raw.length, i = 0, data = ''; i < l; i++) data += String.fromCharCode(raw.charCodeAt(i) & 0xff);
           callback({id: request.id, data: data, type: request.type, size: data.length, name: request.name, url: request.url});
+
         }else{
-        
           var bb = new BlobBuilder();//this webworker is totally overkill
-          bb.append("onmessage = function(e) { for(var raw = e.data, l = raw.length, i = 0, data = ''; i < l; i++) data += String.fromCharCode(raw.charCodeAt(i) & 0xff); postMessage(data) }");
+          bb.append("onmessage = function (e) { for (var raw = e.data, l = raw.length, i = 0, data = ''; i < l; i++) data += String.fromCharCode(raw.charCodeAt(i) & 0xff); postMessage(data); }");
           var url;
-          if(window.createObjectURL){
-            url = window.createObjectURL(bb.getBlob())
-          }else if(window.createBlobURL){
-            url = window.createBlobURL(bb.getBlob())
-          }else if(window.URL && window.URL.createObjectURL){
-            url = window.URL.createObjectURL(bb.getBlob())
-          }else if(window.webkitURL && window.webkitURL.createObjectURL){
-            url = window.webkitURL.createObjectURL(bb.getBlob())
+          if (window.createObjectURL) {
+            url = window.createObjectURL(bb.getBlob());
+          } else if (window.createBlobURL) {
+            url = window.createBlobURL(bb.getBlob());
+          } else if (window.URL && window.URL.createObjectURL) {
+            url = window.URL.createObjectURL(bb.getBlob());
+          } else if (window.webkitURL && window.webkitURL.createObjectURL) {
+            url = window.webkitURL.createObjectURL(bb.getBlob());
           }
           var worker = new Worker(url);
-          worker.onmessage = function(e) {
+          worker.onmessage = function (e) {
             var data = e.data;
             callback({id: request.id, data: data, type: request.type, size: data.length, name: request.name, url: request.url});
           };
-          
+
           worker.postMessage(xhr.responseText);
         }
-        
+
         //*/
-      }else if(type == 'raw'){
+      } else if (type === 'raw') {
         var data = xhr.responseText;
         callback({id: request.id, data: data, type: request.type, size: data.length, name: request.name, url: request.url});
-      }else if(type == 'arraybuffer'){
-          var data = xhr.response;
+      } else if (type === 'arraybuffer') {
+        var data = xhr.response;
         callback({id: request.id, data: data, type: request.type, size: data.length, name: request.name, url: request.url});
-      }else{
-          var raw = xhr.responseText;
+      } else {
+        var raw = xhr.responseText;
         callback({id: request.id, data: raw, type: request.type, size: data.length, name: request.name, url: request.url});
       }
     }
@@ -142,34 +141,34 @@ function getURL(type, request, callback, sync){
 }
 
 
-function getText(request, callback){
+function getText(request, callback) {
   getURL('text', request, callback);
 }
 
-function getRaw(request, callback){
+function getRaw(request, callback) {
   getURL('raw', request, callback);
 }
 
-function getBinary(request, callback){
+function getBinary(request, callback) {
   getURL('binary', request, callback);
 }
 
-
-function getBuffer(request, callback){
-    var tmp = new XMLHttpRequest();
-    var abuf = 'responseType' in tmp && 'response' in tmp;
-    console.log('Testing for array bufs', abuf);
-    getURL(abuf?'arraybuffer':'raw', request, function(file){
-        console.log(abuf, file);
-        if(abuf){
-            callback(file)
-        }else{
-            var bin = file.data
-          var arr = new Uint8Array(bin.length);
-          for(var i = 0, l = bin.length; i < l; i++)
-            arr[i] = bin.charCodeAt(i);
-          file.data = arr.buffer;
-          callback(file);
-        }
-    })
+function getBuffer(request, callback) {
+  var tmp = new XMLHttpRequest();
+  var abuf = 'responseType' in tmp && 'response' in tmp;
+  console.log('Testing for array bufs', abuf);
+  getURL(abuf ? 'arraybuffer' : 'raw', request, function (file) {
+    console.log(abuf, file);
+    if (abuf) {
+      callback(file);
+    } else {
+      var bin = file.data;
+      var arr = new Uint8Array(bin.length);
+      for (var i = 0, l = bin.length; i < l; i++) {
+        arr[i] = bin.charCodeAt(i);
+        file.data = arr.buffer;
+        callback(file);
+      }
+    }
+  });
 }
--- cloudsave.js
+++ cloudsave.js
@@ -1,9 +1,7 @@
 var Hosts = {};
 
-
-//todo: add more
 var original = {
-  "image": {
+  'image': {
     picasa: 'Picasa',
     twitpic: 'TwitPic',
     flickr: 'Flickr',
@@ -13,7 +11,7 @@ var original = {
     facebook: 'Facebook',
     imgly: 'Imgly'
   },
-  "all": {
+  'all': {
     box: 'Box.net',
     sugarsync: 'SugarSync',
     dropbox: 'Dropbox',
@@ -25,69 +23,40 @@ var original = {
     skydrive: 'SkyDrive',
     webdav: 'WebDAV'
   }, 
-  "link": {
+  'link': {
     dropdo: 'Dropdo' //this one is peculiar because it works differently from all the other hosts
   }
 };
-
-
 var additional = {
-    "image": {
-        imageshack: 'Imageshack',
-        imgur: 'Imgur',
-        immio: 'Imm.io'
-    },
-    "all": {
-        hotfile: 'Hotfile'
-    }
+  'image': {
+    imageshack: 'Imageshack',
+    imgur: 'Imgur',
+    immio: 'Imm.io'
+  },
+  'all': {
+    hotfile: 'Hotfile'
+  }
 };
-
-var classes = clone_r(original);
-
-function clone(obj){ //very shallow cloning
-  var n = {};
-  for(var i in obj) n[i] = obj[i]; //we are the knights who say ni!
-  return n;
-}
-
-function clone_r(obj){ //not so shallow cloning
-    if(typeof obj != 'object') return obj;
-  var n = {};
-  for(var i in obj) n[i] = clone(obj[i]); //we are the knights who say ni!
-  return n;
-}
-
-
-
-
-//an order which shoudl theoretically work, but isnt optimal
-//in any stretch of the imagination
-/*
-  general idea: 
-    1. quantity (2 > 1)
-    2. position (end > beginning)
-*/
-
 
 try {
   recent = JSON.parse(localStorage.cloudsave_recent);
-}catch(err){
+} catch (err) {
   recent = [
-    'gdocs',
-    'facebook',
-    'dropbox',
-    'flickr',
-    'box',
-    'clouddrive',
-    'picasa',
+    'hotfile',
     'gdocs',
     'dropbox'
   ];
 }
 
+String.prototype.endsWith = function (s) {
+  return this.length >= s.length && this.substr(this.length - s.length) === s;
+};
 
-function handle_click(tab){
+function handle_click(tab) {
   var url = tab.url;
+  if (url.endsWith('.pdf+html')) {
+    url = tab.url.substr(0, tab.url.length - 5);
+  }
   console.log('Source URL', url);
   var name = unescape(decodeURIComponent(
                       unescape(unescape(unescape(url)))
@@ -100,143 +69,141 @@ function handle_click(tab){
   console.log('Processed name', name);
 
   var host = localStorage.service || 'dropbox';
-  if(Hosts[host]){
-        if(host == 'dropdo'){
-          chrome.tabs.create({
-            url: 'http://dropdo.com/upload?link='+url
-          })
-        }else{
-          if(host == 'dropbox' && localStorage.folder_prefix){
-            name = localStorage.folder_prefix + name;
-          }
-          
-          if(name.indexOf('/') != -1){
-            localStorage.folder_prefix = name.replace(/[^\/]+$/,'');
-          }
-          
-          upload(host, url, name);
-        }
-        console.log(host, url, name);
-        recent.push(host);
-        recent.shift();
-        localStorage.cloudsave_recent = JSON.stringify(recent);
-  }else{
-        alert("Could not find host "+host);
+  if (Hosts[host]) {
+
+    if (host === 'dropbox' && localStorage.folder_prefix) {
+      name = localStorage.folder_prefix + name;
+    }
+
+    if (name.indexOf('/') !== -1) {
+      localStorage.folder_prefix = name.replace(/[^\/]+$/, '');
+    }
+    
+    upload(host, url, name);
+    console.log(host, url, name);
+
+    recent.push(host);
+    //recent.shift();
+    localStorage.cloudsave_recent = JSON.stringify(recent);
+
+  } else {
+    alert('Could not find host ' + host);
   }
 }
 
-var title_map = {};
-var menu_ids = {};
 
-function open_settings(){
-    chrome.tabs.create({url: "settings.html"})
+function open_settings() {
+  chrome.tabs.create({url: 'settings.html'});
 }
 
 //shamelessly stolen from john resig.
 function wbr(str, num) {  
-  return str.replace(RegExp("(\\w{" + num + "})(\\w)", "g"), function(all,text,char){ 
-    return text + "<wbr>" + char; 
+  return str.replace(RegExp('(\\w{' + num + '})(\\w)', 'g'), function(all,text,char){ 
+    return text + '<wbr>' + char; 
   }); 
 }
 
 var INDETERMINATE = {};
 
-
-function updateNotification(id, arg1, arg2){
-    function main(){
-        var wins = chrome.extension.getViews({type:"notification"})
-        var matches = wins.filter(function(win) {
-            return win.location.search.substr(1) == id
-        });
-        if(id == 42) matches = wins; //please coding gods dont kill me
-        if(matches.length){
-            if(typeof arg1 == 'number' || arg1 == INDETERMINATE){
-                matches[0].document.getElementById('progress').style.display = '';
-                matches[0].document.getElementById('progress').value = arg1 == INDETERMINATE ? null : arg1;
-            }else if(arg2){
-                matches[0].document.getElementById('status').innerHTML = arg2;
-                matches[0].document.body.style.backgroundImage = 'url('+arg1+')';
-                matches[0].document.getElementById('progress').style.display = 'none'
-            }else{
-                matches[0].document.getElementById('status').innerHTML = arg1;
-            }
-        }else{
-            return false
-        }
-        return true
+function updateNotification(id, arg1, arg2) {
+  function main() {
+    var wins = chrome.extension.getViews({type: 'notification'});
+    var matches = wins.filter(function (win) {
+      return win.location.search.substr(1) === id;
+    });
+    if (id === 42) matches = wins; //please coding gods dont kill me
+    if (matches.length) {
+      if (typeof arg1 === 'number' || arg1 === INDETERMINATE) {
+        matches[0].document.getElementById('progress').style.display = '';
+        matches[0].document.getElementById('progress').value = arg1 === INDETERMINATE ? null : arg1;
+      } else if (arg2) {
+        matches[0].document.getElementById('status').innerHTML = arg2;
+        matches[0].document.body.style.backgroundImage = 'url(' + arg1 + ')';
+        matches[0].document.getElementById('progress').style.display = 'none';
+      } else {
+        matches[0].document.getElementById('status').innerHTML = arg1;
+      }
+    } else {
+      return false;
     }
-    if(!main()){
-        console.log('Error! Could not locate notification', id, arg1, arg2);
-        var count = 0;
-        function looper(){
-            if(!main() && count++ < 100) setTimeout(looper, 10);
-        }
-        looper();
+    return true;
+  }
+  if (!main()) {
+    console.log('Error! Could not locate notification', id, arg1, arg2);
+    var count = 0;
+    function looper() {
+      if (!main() && count++ < 100) setTimeout(looper, 10);
     }
+    looper();
+  }
 }
 
 
 var urlid = {
-    'todo_fix_this': 42
-    //this is a sort of hack. it uses the file download urls
-    //as a sort of state callback whatnot stuff.
+  'todo_fix_this': 42
+  //this is a sort of hack. it uses the file download urls
+  //as a sort of state callback whatnot stuff.
 };
 
-function uploadProgress(url, event){
-    updateNotification(urlid[url], event.loaded/event.total/2 + 0.5);
+function uploadProgress(url, event) {
+  updateNotification(urlid[url], event.loaded/event.total/2 + 0.5);
 }
 
-function downloadProgress(url, event){
-    updateNotification(urlid[url], event.loaded/event.total/2);
+function downloadProgress(url, event) {
+  updateNotification(urlid[url], event.loaded/event.total/2);
 }
 
 
-function upload(host, url, name){
-    var id = Math.random().toString(36).substr(3);
-    var notification = webkitNotifications.createHTMLNotification('popup.html?'+id);
-    notification.ondisplay = function(){
-        updateNotification(id, 'icon/throbber.gif', 
-            "The file '"+wbr(name,8)+"' is being saved to "+title_map[host]+"...");
-      updateNotification(id, INDETERMINATE);
+function upload(host, url, name) {
+  var id = Math.random().toString(36).substr(3);
+
+  var notification = webkitNotifications.createHTMLNotification('popup.html?' + id);
+
+  notification.ondisplay = function () {
+    updateNotification(id, 'icon/throbber.gif', 
+      'The file "' + wbr(name, 8) + '" is being saved to "' + host + '" ...');
+    updateNotification(id, INDETERMINATE);
+  };
+  var has_uploaded = false;
+  var upload_callback = function () {};
+
+  notification.onclick = function () {
+    if (has_uploaded) {
+      openFile();
+    } else {
+      updateNotification(id, 'Opening file "' + wbr(name, 8) + '" on "' + host + '" in a few seconds...');
+      upload_callback = openFile;
     }
-    var has_uploaded = false;
-    var upload_callback = function(){};
-    
-    notification.onclick = function(){
-        if(has_uploaded){
-            openFile()
-        }else{
-            updateNotification(id, "Opening file '"+wbr(name,8)+"' on "+title_map[host] +" in a few seconds...");
-            upload_callback = openFile;
-        }
-    }
-    notification.onclose = function(){
-        delete urlid[url];
-    }
-    
-    function openFile(){
-        chrome.tabs.create({url: has_uploaded})
-    }
-    notification.show();
-    urlid[url] = id;
+  };
+  notification.onclose = function () {
+    delete urlid[url];
+  };
+
+  function openFile() {
+    chrome.tabs.create({url: has_uploaded});
+  }
+
+  notification.show();
+  urlid[url] = id;
+
   Hosts[host]({
     url: url,
     name: name
-  }, function(e){
-      has_uploaded = e && e.url;
-      setTimeout(upload_callback, 200);
+  }, function (e) {
+    has_uploaded = e && e.url;
+    setTimeout(upload_callback, 200);
     console.log('uploaded file yay', e);
-    if(e && typeof e == "string" && e.indexOf('error:') != -1){
+    if (e && typeof e === "string" && e.indexOf('error:') !== -1) {
       updateNotification(id, 'icon/64sad.png', 
-        "The file '"+wbr(name,8)+"' could not be uploaded to "+
-        title_map[host]+". "+e.substr(6));
-    }else{
-        updateNotification(id, 'icon/64.png', 
-            "The file '"+wbr(name,8)+"' has been uploaded to "+title_map[host]+"."
+        'The file "' + wbr(name, 8) + '" could not be uploaded to "' +
+        host + '". ' + e.substr(6));
+    } else {
+      updateNotification(id, 'icon/64.png', 
+        'The file "' + wbr(name, 8) + '" has been uploaded to "' + host + '".'
         );
-        setTimeout(function(){
-            notification.cancel();
-            }, 5.4 * 1000) //May the fourth be with you.
+      setTimeout(function () {
+        notification.cancel();
+      }, 5.4 * 1000); //May the fourth be with you.
     }
-  })
+  });
 }
